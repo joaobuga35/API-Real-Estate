ab704bdc152d5d432954e9bdc0f0b25b26ef74ed
joaobuga35 <joaobuga35@gmail.com>
2023-03-05 23:21:39 -0300

    environment is completed

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,10 @@
+import "express-async-errors";
 import express, { Application } from "express";
+import { handleError } from "./errors";
 
 const app: Application = express();
 app.use(express.json());
+
+app.use(handleError);
 
 export default app;
