dd2ef6bcd7acb7dbc16e019586a35e9b1f1b8be0
joaobuga35 <joaobuga35@gmail.com>
2023-03-07 16:45:54 -0300

    create update route, missing verify user is adm or not

--- src/controllers/users.controllers.ts
+++ src/controllers/users.controllers.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import createUserService from "../services/Users/createUser.service";
 import readUsersService from "../services/Users/readUsers.service";
+import updateUserService from "../services/Users/updateUser.service";
 
 const createUserController = async (req: Request, resp: Response): Promise<Response> => {
 
@@ -10,11 +11,29 @@ const createUserController = async (req: Request, resp: Response): Promise<Respo
 };
 
 const readUsersController = async (req: Request, resp: Response): Promise<Response> => {
+
 	const allUsers = await readUsersService();
+
 	return resp.status(200).json(allUsers);
 };
 
+const updateUsersController = async (req: Request, resp: Response): Promise<Response> => {
+
+	const id = Number(req.params.id);
+
+	const newUser = await updateUserService(req.body, id);
+
+	return resp.status(200).json(newUser);
+};
+
+const deleteUsersController = async (req: Request, resp: Response): Promise<Response> => {
+	return resp.status(204).json();
+};
+
+
 export {
 	createUserController,
-	readUsersController
+	readUsersController,
+	updateUsersController,
+	deleteUsersController
 };
---
+++ src/services/Users/updateUser.service.ts
@@ -0,0 +1,27 @@
+import { Repository } from "typeorm";
+import { AppDataSource } from "../../data-source";
+import { User } from "../../entities";
+import { iReturnCreateUser, iUpdateUsersPartial } from "../../interfaces/users.interface";
+import { returnCreateUserSchema } from "../../schemas/users.schema";
+
+const updateUserService = async (userData: iUpdateUsersPartial, idUser: number): Promise<iReturnCreateUser> => {
+
+	const userRepo: Repository<User> = AppDataSource.getRepository(User);
+
+	const oldUserData: User | null = await userRepo.findOneBy({
+		id: idUser
+	});
+
+	const newUser = userRepo.create({
+		...oldUserData,
+		...userData
+	});
+
+	await userRepo.save(newUser);
+
+	const updatedUser = returnCreateUserSchema.parse(newUser);
+
+	return updatedUser;
+};
+
+export default updateUserService;
